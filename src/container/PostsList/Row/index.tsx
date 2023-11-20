34e0dbb31f67f07809ba038928194ceb9435156b
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-20 20:32:30 +0300

    add Row component

---
+++ src/container/PostsList/Row/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import styles from "./styles.module.scss";
+
+type PropsType = {
+  id: number;
+  body: string;
+  title: string;
+};
+
+export const Row: FC<PropsType> = ({ id, body, title }) => {
+  return (
+    <div className={styles.row}>
+      <div>{id}</div>
+      <div>{body}</div>
+      <div>{title}</div>
+      <div>
+        <button
+          className={styles.row__btn}
+          onClick={() => {}}
+        >
+          Просмотр
+        </button>
+      </div>
+    </div>
+  );
+};
