6ca055d609f836dcd073a634f10cf0030acc90cf
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-21 17:58:26 +0300

    add postApi

---
+++ src/API/post.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Post } from "types";
+
+export const postApi = createApi({
+  reducerPath: "postApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com/",
+  }),
+  endpoints: builder => ({
+    getPosts: builder.query<Post, { limit: number; page: number }>({
+      query: ({ limit, page }) => `posts?page=${page}&_limit=${limit}`,
+    }),
+  }),
+});
+
+export const { useGetPostsQuery } = postApi;
