65cc6c29c89ed5f707535627d135564961f55baa
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-22 15:22:39 +0300

    add scroll pagination to PostList

--- src/container/PostsList/index.tsx
+++ src/container/PostsList/index.tsx
@@ -1,26 +1,54 @@
-import { useEffect, useState } from "react";
-import { Row } from "./Row";
+import { useEffect, useRef, useState } from "react";
+import { Post } from "types";
+import { useGetPostsQuery } from "API/post";
+import { Loader } from "components";
 import styles from "./styles.module.scss";
+import { Row } from "./Row";
 
-export interface RootInterface {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
 export const PostsList = () => {
-  const [posts, setPosts] = useState([] as RootInterface[]);
+  const [posts, setPosts] = useState([] as Post[]);
+  const [page, setPage] = useState(1);
+  const bodyRef = useRef<HTMLDivElement>(null);
 
-  const fetchPosts = async () => {
-    const data: Promise<RootInterface[]> = (
-      await fetch("https://jsonplaceholder.typicode.com/posts?_page=1_limit=20")
-    ).json();
-    setPosts(await data);
-  };
+  const { data, isFetching, isLoading } = useGetPostsQuery({ limit: 20, page });
+
+  // Проверка есть ли еще посты для загрузки
+  const hasMore = data ? data[data.length - 1].id < 100 : false;
+
+  const isLoaderShown = hasMore || isLoading;
 
   useEffect(() => {
-    fetchPosts();
-  }, []);
+    const onScroll = () => {
+      const scrollHeight = bodyRef.current?.scrollHeight;
+      const scrollTop = bodyRef.current?.scrollTop;
+      const offsetHeight = bodyRef.current?.offsetHeight;
+
+      // Проверка достигнута ли нижняя граница при скролле
+      let scrolledToBottom;
+      if (scrollHeight && scrollTop && offsetHeight)
+        scrolledToBottom = offsetHeight + scrollTop >= scrollHeight;
+
+      if (scrolledToBottom && !isFetching && hasMore) {
+        setPage(prev => prev + 1);
+      }
+    };
+
+    bodyRef.current!.addEventListener("scroll", onScroll);
+
+    return () => {
+      bodyRef.current?.removeEventListener("scroll", onScroll);
+    };
+  }, [page, isFetching]);
+
+  useEffect(() => {
+    if (data) setPosts(prev => [...prev, ...data]);
+  }, [data]);
+
+  /* 
+  Добавляем этот useEffect, т.к. при возвращении после страницы post 
+  предыдущие посты остаются и продолжают накапливаться
+  */
+  useEffect(() => () => setPosts([]), []);
 
   return (
     <div className={styles.post_list}>
@@ -29,13 +57,21 @@ export const PostsList = () => {
         <div>Description</div>
         <div>Title</div>
       </div>
-      <div className={styles.post_list__body}>
+      <div
+        className={styles.post_list__body}
+        ref={bodyRef}
+      >
         {posts.map(post => (
           <Row
             key={post.id}
             {...post}
           />
         ))}
+        {isLoaderShown && (
+          <div className={styles.post_list__loader}>
+            <Loader />
+          </div>
+        )}
       </div>
     </div>
   );
