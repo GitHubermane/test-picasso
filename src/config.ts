f841e9a68fc15f53a38b56b5be462cbdb65fed61
Viktor Kryuchkov <crykour12@gmail.com>
2023-11-22 17:12:59 +0300

    add urlpath for react router dom

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const URLpath = "/test-picasso/";
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { URLpath } from "config.ts";
+
 import { Provider } from "react-redux";
 import { store } from "store";
 
@@ -13,7 +15,7 @@ import "normalize.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={URLpath}>
         <App />
       </BrowserRouter>
     </Provider>
